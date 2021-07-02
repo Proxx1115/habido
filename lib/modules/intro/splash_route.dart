909b00c16981f5e51f7ed6515baf5a19dd93fb35
Jargalbat <jargalbat@gmail.com>
2021-07-02 17:56:48 +0800

    null safety

---
+++ lib/modules/intro/splash_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashRoute extends StatefulWidget {
+  @override
+  _SplashRouteState createState() => _SplashRouteState();
+}
+
+class _SplashRouteState extends State<SplashRoute> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
