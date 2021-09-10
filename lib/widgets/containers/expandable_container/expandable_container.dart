efe41ca820b641ae312b415445ab45e0affad339
Jargalbat <jargalbat@gmail.com>
2021-09-10 11:22:35 +0800

    changes

--- lib/widgets/containers/expandable_container/expandable_container.dart
+++ lib/widgets/containers/expandable_container/expandable_container.dart
@@ -31,13 +31,15 @@ class _ExpandableContainerState extends State<ExpandableContainer> {
 
   @override
   void initState() {
-    _expandedHeight = widget.expandableListItems.length * (_listItemHeight + _liteItemMarginBottom);
+
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    _expandedHeight = widget.expandableListItems.length * (_listItemHeight + _liteItemMarginBottom);
+
     return Column(
       children: [
         /// Header
@@ -106,32 +108,3 @@ class _ExpandableContainerState extends State<ExpandableContainer> {
     );
   }
 }
-
-// class ExpandableContainer extends StatelessWidget {
-//   final bool expanded;
-//   final double collapsedHeight;
-//   final double expandedHeight;
-//   final Widget child;
-//
-//   ExpandableContainer({
-//     required this.child,
-//     this.collapsedHeight = 0.0,
-//     this.expandedHeight = 300.0,
-//     this.expanded = true,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     double screenWidth = MediaQuery.of(context).size.width;
-//     return new AnimatedContainer(
-//       duration: new Duration(milliseconds: 500),
-//       curve: Curves.easeInOut,
-//       width: screenWidth,
-//       height: expanded ? expandedHeight : collapsedHeight,
-//       child: new Container(
-//         child: child,
-//         decoration: new BoxDecoration(border: new Border.all(width: 1.0, color: Colors.blue)),
-//       ),
-//     );
-//   }
-// }
