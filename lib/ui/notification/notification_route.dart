ebb8d5c14f7af1eeafd2ec4cb4f9747dd41af2cb
Jargalbat <jargalbat@gmail.com>
2021-09-29 14:16:34 +0800

    changes

--- lib/ui/notification/notification_route.dart
+++ lib/ui/notification/notification_route.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart' as cupertino;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,11 +6,16 @@ import 'package:habido_app/bloc/bloc_manager.dart';
 import 'package:habido_app/bloc/notification_bloc.dart';
 import 'package:habido_app/models/notif.dart';
 import 'package:habido_app/utils/assets.dart';
+import 'package:habido_app/utils/func.dart';
 import 'package:habido_app/utils/localization/localization.dart';
+import 'package:habido_app/utils/size_helper.dart';
+import 'package:habido_app/utils/theme/custom_colors.dart';
 import 'package:habido_app/utils/theme/hex_color.dart';
+import 'package:habido_app/widgets/animations/animations.dart';
 import 'package:habido_app/widgets/containers/containers.dart';
 import 'package:habido_app/widgets/dialogs.dart';
 import 'package:habido_app/widgets/scaffold.dart';
+import 'package:habido_app/widgets/text.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class NotificationRoute extends StatefulWidget {
@@ -46,65 +52,46 @@ class _NotificationRouteState extends State<NotificationRoute> {
               return SmartRefresher(
                 enablePullDown: true,
                 enablePullUp: true,
-                header: WaterDropHeader(),
+                header: ClassicHeader(
+                  refreshStyle: RefreshStyle.Follow,
+                  idleText: 'asdf',
+                  idleIcon: Icon(Icons.expand_more, color: Colors.grey),
+                  releaseText: "",
+                  refreshingText: "",
+                  completeText: "",
+//            completeIcon: null,
+                ),
+                // header: WaterDropHeader(
+                //
+                // ),
+
                 footer: CustomFooter(
                   builder: (BuildContext context, LoadStatus? mode) {
                     Widget body;
                     if (mode == LoadStatus.idle) {
-                      body = Text("pull up load");
+                      body = Container(); // pull up load
                     } else if (mode == LoadStatus.loading) {
                       body = cupertino.CupertinoActivityIndicator();
                     } else if (mode == LoadStatus.failed) {
-                      body = Text("Load Failed!Click retry!");
+                      body = Container(); // Load Failed! Click retry!
                     } else if (mode == LoadStatus.canLoading) {
-                      body = Text("release to load more");
+                      body = Container(); // release to load more
                     } else {
-                      body = Text("No more Data");
+                      body = Container(); // No more Data
                     }
                     return Container(
                       height: 55.0,
                       child: Center(child: body),
                     );
                   },
-
-                  // builder: (BuildContext context, LoadStatus mode) {
-                  //   Widget body;
-                  //   if (mode == LoadStatus.idle) {
-                  //     body = Text("pull up load");
-                  //   } else if (mode == LoadStatus.loading) {
-                  //     body = CupertinoActivityIndicator();
-                  //   } else if (mode == LoadStatus.failed) {
-                  //     body = Text("Load Failed!Click retry!");
-                  //   } else if (mode == LoadStatus.canLoading) {
-                  //     body = Text("release to load more");
-                  //   } else {
-                  //     body = Text("No more Data");
-                  //   }
-                  //   return Container(
-                  //     height: 55.0,
-                  //     child: Center(child: body),
-                  //   );
-                  // },
-                  //
                 ),
                 controller: _refreshController,
                 onRefresh: _onRefresh,
                 onLoading: _onLoading,
                 child: ListView.builder(
-                  itemBuilder: (context, index) => ListItemContainer(
-                    margin: EdgeInsets.only(bottom: 10.0),
-                    height: 70.0,
-                    leadingImageUrl: _notifList[index].photo,
-                    leadingBackgroundColor: HexColor.fromHex(_notifList[index].color ?? '#F4F6F8'),
-                    title: _notifList[index].title ?? '',
-                    body: _notifList[index].body,
-                    onPressed: () {
-                      // Navigator.pushNamed(context, Routes.psyIntro, arguments: {
-                      //   'psyTest': el,
-                      // });
-                    },
-                  ),
-                  itemExtent: 100.0,
+                  itemBuilder: (context, index) => _listItem(index),
+                  // itemExtent: 90.0,
+
                   itemCount: _notifList.length,
                 ),
               );
@@ -130,10 +117,78 @@ class _NotificationRouteState extends State<NotificationRoute> {
     }
   }
 
+  Widget _listItem(int index) {
+    return MoveInAnimation(
+      delay: Func.toDouble(index * 0.1),
+      child: ListItemContainer(
+        margin: EdgeInsets.fromLTRB(SizeHelper.padding, SizeHelper.padding, SizeHelper.padding, 0.0),
+        height: 70.0,
+        leadingImageUrl: _notifList[index].photo,
+        leadingBackgroundColor: HexColor.fromHex(_notifList[index].color ?? '#F4F6F8'),
+        title: _notifList[index].title ?? '',
+        body: _notifList[index].body,
+        onPressed: () {
+          showCustomDialog(
+            context,
+            child: CustomDialogBody(
+              buttonText: LocaleKeys.ok,
+              child: Column(
+                children: [
+                  /// Image
+                  if (Func.isNotEmpty(_notifList[index].photo))
+                    Container(
+                      margin: EdgeInsets.only(right: 15.0),
+                      padding: EdgeInsets.all(10.0),
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(SizeHelper.borderRadius)),
+                        color: customColors.greyBackground,
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: _notifList[index].photo!,
+                        placeholder: (context, url) => Container(),
+                        //CustomLoader(),
+                        errorWidget: (context, url, error) => Container(),
+                        height: 40.0,
+                        // width: 40.0,
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+
+                  /// Title
+                  CustomText(
+                    _notifList[index].title,
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(vertical: 20.0),
+                    fontWeight: FontWeight.w500,
+                  ),
+
+                  /// Body
+                  CustomText(
+                    _notifList[index].body,
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    maxLines: 10,
+                    color: customColors.secondaryText,
+                  ),
+                ],
+              ),
+              onPressedButton: () {},
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   void _onRefresh() async {
     // monitor network fetch
     await Future.delayed(Duration(milliseconds: 1000));
     // if failed,use refreshFailed()
+
+    _notifList = [];
+    BlocManager.notifBloc.add(GetFirstNotifsEvent());
+
     _refreshController.refreshCompleted();
   }
 
